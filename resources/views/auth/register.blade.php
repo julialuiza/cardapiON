572fbdabe108a0b99690fdfd4f943bfcd78d316c
SthePasso <sjgp@icomp.ufam.edu.br>
2019-12-10 18:27:55 -0400

    Cadastros

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -25,6 +25,11 @@
                     </div>
 
                     <div class="form-label-group">
+                      <input type="text" id="bio" name="bio"  class="form-control" placeholder="Bio" value="" required autofocus>
+                      <label for="nome">Uma frase que me defina</label>
+                    </div>
+
+                    <div class="form-label-group">
                       <input type="password" id="senha" name="senha" class="form-control" placeholder="Sua senha secreta" value="" required>
                       <label for="Senha">Senha</label>
                     </div>
