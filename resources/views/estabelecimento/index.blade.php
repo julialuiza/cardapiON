06ccb71aab419236134736ac256c9d0d4865681d
Júlia Luiza <jlslc@icomp.ufam.edu.br>
2019-12-13 10:06:46 -0300

    Front-end da pagina de feed

--- resources/views/estabelecimento/index.blade.php
+++ resources/views/estabelecimento/index.blade.php
@@ -2,14 +2,83 @@
 
 @section('content')
 
-<div class="container-fluid">
-    <div class="row no-gutter">
-      <div class="col">
-        <div class="text-justify-center">
-            <h3 class="login-heading">Cardapi<strong class="text-danger">ON</strong></h3>
-            <h5>Feed de Cardápios</h5>
-        </div>
-      </div>
-    </div>
+<div class="container mt-3 d-flex justify-content-center">
+	<div class="row mt-3">
+	  <div class="col-12">
+		 <h3 class="text-center">Cardárpios Disponíveis</h3>      
+	  </div>
+	  <div class="col-12">
+		<!-- form de busca-->
+		<form method="POST" action="#">
+		  <input class="form-control" type="search" placeholder="Nome do estabelecimento, tipo de comida, pratos.." aria-label="Pesquisar">
+		  <button class="btn btn-sm btn-warning btn-block" type="submit">Pesquisar</button>
+		</form>
+	  </div> 
+	</div>
+</div>
+ 
+
+<div class="container mt-3">
+	<div class="row mt-3">
+		<div class="col ">
+		  <div class="card-deck">
+			<div class="card">
+			  <img src="imgs/estab.png" class="card-img-top" alt="...">
+			  <div class="card-body">
+				<h5 class="card-title">Nome Restaurante</h5>
+				<p class="card-text">
+				  <b>Localização:</b> {} <br>
+				  <b>Nota média:</b> {}
+				</p>
+			  </div>
+			  <div class="card-footer bg-black">
+				<button class="btn btn-sm btn-block btn-outline-dark text-white">VER MAIS</button>
+			  </div>
+			</div>
+
+			<div class="card">
+			  <img src="imgs/estab.png" class="card-img-top" alt="...">
+			  <div class="card-body">
+				<h5 class="card-title">Nome Restaurante</h5>
+				<p class="card-text">
+				  <b>Localização:</b> {} <br>
+				  <b>Nota média:</b> {}
+				</p>
+			  </div>
+			  <div class="card-footer bg-black">
+				<button class="btn btn-sm btn-block btn-outline-dark text-white">VER MAIS</button>
+			  </div>
+			</div>
+
+			<div class="card">
+			  <img src="imgs/estab.png" class="card-img-top" alt="...">
+			  <div class="card-body">
+				<h5 class="card-title">Nome Restaurante</h5>
+				<p class="card-text">
+				  <b>Localização:</b> {} <br>
+				  <b>Nota média:</b> {}
+				</p>
+			  </div>
+			  <div class="card-footer bg-black">
+				<button class="btn btn-sm btn-block btn-outline-dark text-white">VER MAIS</button>
+			  </div>
+			</div>
+
+			<div class="card">
+			  <img src="imgs/estab.png" class="card-img-top" alt="...">
+			  <div class="card-body">
+				<h5 class="card-title">Nome Restaurante</h5>
+				<p class="card-text">
+				  <b>Localização:</b> {} <br>
+				  <b>Nota média:</b> {}
+				</p>
+			  </div>
+			  <div class="card-footer bg-black">
+				<button class="btn btn-sm btn-block btn-outline-dark text-white">VER MAIS</button>
+			  </div>
+			</div>
+		  </div>
+		</div>
+	</div>
 </div>
 @endsection
