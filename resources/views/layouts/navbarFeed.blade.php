a9605b945f6d81861d1e50fc07da73e36de4706e
Júlia Luiza <jlslc@icomp.ufam.edu.br>
2019-12-13 10:04:57 -0300

    Update navbarFeed.blade.php

--- resources/views/layouts/navbarFeed.blade.php
+++ resources/views/layouts/navbarFeed.blade.php
@@ -19,7 +19,7 @@
 </head>
 <body>
   <!-- Navigation -->
-  <nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
+  <nav class="navbar navbar-expand-lg navbar-dark navbar-custom">
     <div class="container">
       <a class="navbar-brand" href="{{ url('/') }}">cardapi<strong class="text-danger">ON</strong></a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
